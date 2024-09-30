6024eaefa9c406d32106e5148c59ef56cc020b62
Hatem <moh.medh.tem1@gmail.com>
2024-09-30 20:42:38 +0300

    initial

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,56 +2,79 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
-import guru.springframework.spring6webapp.repository.AuthorRepository;
-import guru.springframework.spring6webapp.repository.BookRepository;
+import guru.springframework.spring6webapp.domain.Publisher;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Component
 public class BootstrapData implements CommandLineRunner {
+
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
         Author eric = new Author();
-        eric.setFirstName("eric");
-        eric.setLastName("eric last name ");
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
 
-        Book firstBook = new Book();
-        firstBook.setName("firstBook");
-        firstBook.setIsbn("1245");
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
 
-        Author savedEric = authorRepository.save(eric);
-        Book savedFirstBook = bookRepository.save(firstBook);
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
 
-        Author secondAuthor = new Author();
-        eric.setFirstName("eric");
-        eric.setLastName("eric last name ");
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
 
-        Book secondBook = new Book();
-        firstBook.setName("secondBook");
-        firstBook.setIsbn("1234");
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE Development without EJB");
+        noEJB.setIsbn("54757585");
 
-        Author savedsecondAuthor = authorRepository.save(secondAuthor);
-        Book savedSecondBook = bookRepository.save(secondBook);
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
 
-        savedEric.getBooks().add(savedFirstBook);
-        savedsecondAuthor.getBooks().add(savedSecondBook);
+        ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
 
-        authorRepository.save(savedEric);
-        authorRepository.save(savedsecondAuthor);
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+        
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
 
-        System.out.println("from bootStrap ");
-        System.out.println("author count ; " + authorRepository.count());
-        System.out.println("book count ; " + bookRepository.count());
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        publisherRepository.save(publisher);
 
-
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
+
+
+
+
+
+
+
+
+
+
