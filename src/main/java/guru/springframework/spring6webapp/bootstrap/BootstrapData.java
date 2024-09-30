b4c09e1208c8170f095df03c240e60d4cf0dbc46
Hatem <moh.medh.tem1@gmail.com>
2024-09-30 08:29:01 +0300

    add BootstrapData.java

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repository.AuthorRepository;
+import guru.springframework.spring6webapp.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author();
+        eric.setFirstName("eric");
+        eric.setLastName("eric last name ");
+
+        Book firstBook = new Book();
+        firstBook.setName("firstBook");
+        firstBook.setIsbn("1245");
+
+        Author savedEric = authorRepository.save(eric);
+        Book savedFirstBook = bookRepository.save(firstBook);
+
+        Author secondAuthor = new Author();
+        eric.setFirstName("eric");
+        eric.setLastName("eric last name ");
+
+        Book secondBook = new Book();
+        firstBook.setName("secondBook");
+        firstBook.setIsbn("1234");
+
+        Author savedsecondAuthor = authorRepository.save(secondAuthor);
+        Book savedSecondBook = bookRepository.save(secondBook);
+
+        savedEric.getBooks().add(savedFirstBook);
+        savedsecondAuthor.getBooks().add(savedSecondBook);
+
+        authorRepository.save(savedEric);
+        authorRepository.save(savedsecondAuthor);
+
+        System.out.println("from bootStrap");
+        System.out.println("author count ; " + authorRepository.count());
+        System.out.println("book count ; " + bookRepository.count());
+
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,76 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+}
